2bf5d1d719cee5390960dea6a7c079b139c23eec
Nawale Rajat Gitaram <96673505+nawalerajat20@users.noreply.github.com>
2024-04-04 09:17:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Feedback from './Components/Feedback';
 import Home from './Components/Home';
 
 import Cryptoinfo from './Components/Cryptoinfo';
-import Report_a_bug from './Components/Report_a_bug';
+//import Report_a_bug from './Components/Report_a_bug';
 import FAQ from './Components/FAQ';
 import Predictions from './Components/Predictions';
 import Footer from './Components/Footer';
 // import Table from './Components/Table';
 import News from './Components/News';
 // import Userprofile from './Components/Userprofile';
-import './Report_a_bug.css'
+//import './Report_a_bug.css'
 import './Navbar.css';
 import './Home.css'
 import './App.css';
@@ -37,7 +37,7 @@ function App() {
             <Route exact path='/predictions' element={<Predictions/>}></Route>
             <Route exact path='/faqs' element={<FAQ/>}></Route>
             <Route exact path='/news' element={<News/>}></Route>
-            <Route exact path='/report_a_bug' element={<Report_a_bug/>}></Route>
+            //<Route exact path='/report_a_bug' element={<Report_a_bug/>}></Route>
             <Route exact path='/privacy' element={<Privacy/>}></Route>
             <Route exact path='/feedback' element={<Feedback/>}></Route>
             {/* <Route exact path='/predictions' element={<Predictions/>}></Route> */}
